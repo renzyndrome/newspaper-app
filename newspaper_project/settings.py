06f066cd542e90f8563907be1dabd2526230239d
Laurence <gabrielrebadulla@gmail.com>
2019-04-16 22:09:08 +0800

    hide email host pw

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -135,8 +135,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_PASSWORD = (config.EMAIL_HOST_PASSWORD)
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+#EMAIL_HOST_PASSWORD = (config.EMAIL_HOST_PASSWORD)
 
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
